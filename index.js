df37225e05fd52b840e2d288e3ca96e5c45c0cc8
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-12 21:04:38 +0500

    Fixed time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let date;
 
 setInterval(() => {
     dateTime = new Date();
-    time = dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + String(dateTime.getSeconds()).padStart(2,'0');
+    time = dateTime.getHours() + ":" + String(dateTime.getMinutes()).padStart(2,'0') + ":" + String(dateTime.getSeconds()).padStart(2,'0');
     date = dateTime.toLocaleDateString();
     document.querySelector("#time").innerHTML = time;
     document.querySelector("#date").innerHTML = date;
