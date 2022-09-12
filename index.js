66e9713d3bf5828f1aed8aed711ae6f41552da74
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-12 11:30:46 +0500

    Added date and time functionality

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let dateTime = new Date();
+let dateTime;
+let time;
+let date;
+
+setInterval(() => {
+    dateTime = new Date();
+    time = dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + String(dateTime.getSeconds()).padStart(2,'0');
+    date = dateTime.toLocaleDateString();
+    document.querySelector("#time").innerHTML = time;
+    document.querySelector("#date").innerHTML = date;
+}, 1000);
